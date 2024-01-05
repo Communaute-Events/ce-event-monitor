d860fa7741a253bfb278893a9e1ba7c28845aef5
OrigamingWasTaken <mabarleycorne@gmail.com>
2024-01-05 18:33:18 +0100

    Added Map detection

--- src/@types/env.d.ts
+++ src/@types/env.d.ts
@@ -3,6 +3,7 @@ export {};
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
+        sourcesUrl: string,
         token: string,
         port: any
     }
--- src/index.ts
+++ src/index.ts
@@ -8,16 +8,20 @@ import fs from "fs"
 // import so typescript includes the file
 import evsrc from "../data/sources.json"
 
-let EventSources = JSON.parse(fs.readFileSync("data/sources.json","utf-8") || "[]")
-fetch("https://raw.githubusercontent.com/Communaute-Events/ce-event-monitor/main/data/sources.json").then(res => res.json().then(res => {
-    EventSources = res
-    console.log("[Event Monitor] Sources loaded.")
-    console.log(EventSources.map(src => `   -> ${src.name}`).join("\n"))
-})).catch(err => {
-    console.log("[Event Monitor] An error occured while fetching event sources. The data/sources.json file will be used.\n\n" + err)
-    console.log("[Event Monitor] Sources loaded.")
-    console.log(EventSources.map(src => `   -> ${src.name}`).join("\n"))
-})
+let EventSources = JSON.parse(fs.readFileSync("data/sources.json", "utf-8") || "[]")
+if (process.env.sourcesUrl) {
+    fetch(process.env.sourcesUrl).then(res => res.json().then(res => {
+        EventSources = res
+        console.log("[Event Monitor] Sources loaded.")
+        console.log(EventSources.map(src => `   -> ${src.name}`).join("\n"))
+    })).catch(err => {
+        console.log("[Event Monitor] An error occured while fetching event sources. The data/sources.json file will be used.\n\n" + err)
+        console.log("[Event Monitor] Sources loaded.")
+        console.log(EventSources.map(src => `   -> ${src.name}`).join("\n"))
+    })
+} else {
+    console.log("[Event Monitor] Sources will be loaded from local sources.json.")
+}
 
 const client = new Client({
     checkUpdate: false
@@ -35,6 +39,12 @@ client.once('ready', async () => {
     console.log(`[Event Monitor] ${client.user.username} is ready!`);
 })
 
+function sendMessage(message: string) {
+    wss.clients.forEach(client => {
+        client.send(message);
+    });
+}
+
 client.on("messageCreate", async (msg) => {
     if (isSelfbot(msg.author.id)) { return; }
     EventSources.forEach((source) => {
@@ -43,26 +53,47 @@ client.on("messageCreate", async (msg) => {
             // @ts-expect-error
             // If mentionned role is in source or there is a @everyone/here
             if (source.roles.some(roleId => msg.toJSON().mentions.roles.includes(roleId)) || msg.content.includes("@here") || msg.content.includes("@everyone")) {
-                wss.clients.forEach(client => {
-                    client.send(JSON.stringify({
-                        _type: "event",
-                        eventSource: source,
-                        message: {
-                            data: msg.toJSON(),
-                            url: msg.url
-                        },
-                        author: {
-                            name: msg.author.displayName,
-                            id: msg.author.id
-                        },
-                        guild: {
-                            name: msg.guild.name,
-                            id: msg.guild.id,
-                            iconUrl: msg.guild.iconURL()
-                        },
-                        timeStamp: new Date().toISOString()
-                    }));
-                });
+                sendMessage(JSON.stringify({
+                    _type: "event",
+                    eventSource: source,
+                    message: {
+                        data: msg.toJSON(),
+                        url: msg.url
+                    },
+                    author: {
+                        name: msg.author.displayName,
+                        id: msg.author.id
+                    },
+                    guild: {
+                        name: msg.guild.name,
+                        id: msg.guild.id,
+                        iconUrl: msg.guild.iconURL()
+                    },
+                    timeStamp: new Date().toISOString()
+                }))
+            }
+        }
+        // Checks for maps && checks for link in message or file
+        if (msg.guild.id === source.guildId && source.admins.includes(msg.author.id) && source.maps.includes(msg.channel.id)) {
+            if (new RegExp("([a-zA-Z0-9]+://)?([a-zA-Z0-9_]+:[a-zA-Z0-9_]+@)?([a-zA-Z0-9.-]+\\.[A-Za-z]{2,4})(:[0-9]+)?(/.*)?").test(msg.content) || msg.attachments.size > 0) {
+                sendMessage(JSON.stringify({
+                    _type: "map",
+                    eventSource: source,
+                    message: {
+                        data: msg.toJSON(),
+                        url: msg.url
+                    },
+                    author: {
+                        name: msg.author.displayName,
+                        id: msg.author.id
+                    },
+                    guild: {
+                        name: msg.guild.name,
+                        id: msg.guild.id,
+                        iconUrl: msg.guild.iconURL()
+                    },
+                    timeStamp: new Date().toISOString()
+                }))
             }
         }
     })
